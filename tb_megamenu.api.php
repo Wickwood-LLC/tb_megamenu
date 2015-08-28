7c9589459467dba82c1d2c82b0ecb91b9196e380
Michael Ngo <michaelngodh@gmail.com>
2015-08-28 17:35:43 +0700

    Create API hook_tb_megamenu_extension_info. Allow user to declare a new extension

--- tb_megamenu.api.php
+++ tb_megamenu.api.php
@@ -1,7 +1,14 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: michael
- * Date: 25/08/2015
- * Time: 16:56
+ * Register extension for TB Mega Menu.
  */
+function hook_tb_megamenu_extension_info() {
+  $example_info = array(
+    'machine_name' => array(
+      'title' => t('Name of extension'),
+      'type' => 'theme'
+    )
+  );
+  return $example_info;
+}
