da47a417fc4c2597504a6e5614d442db7ff8ad07
Michael Ngo <michaelngodh@gmail.com>
2015-09-11 11:04:56 +0700

    create API hooks

--- tb_megamenu.api.php
+++ tb_megamenu.api.php
@@ -4,11 +4,19 @@
  * Register extension for TB Mega Menu.
  */
 function hook_tb_megamenu_extension_info() {
-  $example_info = array(
+  return array(
     'machine_name' => array(
       'title' => t('Name of extension'),
       'type' => 'theme'
     )
   );
-  return $example_info;
+}
+
+/**
+ * Set default value to configuration.
+ */
+function hook_tb_megamenu_block_config_info() {
+  return array(
+    'config-item' => 'value'
+  );
 }
