3f8dbd9d2bd859ce6bb8caee17ffc71e23057cca
Michael Ngo <michaelngodh@gmail.com>
2015-09-21 16:02:43 +0700

    Allow extensions to implement preprocess of templates

--- tb_megmanu_offcanvas/tb_megamenu_offcanvas.js
+++ tb_megmanu_offcanvas/tb_megamenu_offcanvas.js
@@ -1,15 +1,21 @@
 (function ($) {
-
   var OffCanvas = {
     prepareToSave: function () {
-      return {'off-canvas': $("[name='tb-megamenu-off-canvas']:checked").val()};
+      return {
+        'off_canvas' : {
+          'status': $("[name='tb-megamenu-off-canvas']:checked").val(),
+          //'config': JSON.stringify({
+          //  'example': 'Text text'
+          //})
+        }
+      };
     }
   };
 
   Drupal.behaviors.OffCanvas = {
     attach: function () {
       // Get data from Off-canvas.
-      Drupal.TBMegaMenu.registerExtension({'off-canvas': OffCanvas});
+      Drupal.TBMegaMenu.registerExtension({'off_canvas': OffCanvas});
     }
   };
 })(jQuery);
